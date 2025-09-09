96efa320fc4367f8645a831c53192c367323fcad
Alejandro <chapale2000@gmail.com>
2025-09-09 18:19:01 -0300

    clase tree comenzada

--- node.rb
+++ node.rb
@@ -2,10 +2,22 @@ class Node
   include Comparable
   attr_reader :value, :left_node, :right_node
 
-  def initialize(value)
+  def initialize(value, left_node = nil, right_node = nil)
     @value = value
-    @left_node = nil
-    @right_node = nil
+    @left_node = left_node
+    @right_node = right_node
+  end
+
+  def set_value(value)
+    @value = value
+  end
+
+  def set_left(node)
+    @left_node = node
+  end
+
+  def set_right(node)
+    @right_node = node
   end
 
   def <=>(other)
--- tree.rb
+++ tree.rb
@@ -1,6 +1,22 @@
 class Tree 
   
   def initialize(arr)
-    
+    @root = build_tree(arr.sort)
   end
+
+  def build_tree(arr)
+    start = 0
+    final = arr.length - 1
+    return if start > final
+    mid = ((start + final)/2).floor
+    left_tree = build_tree(arr[start..mid])
+    right_tree = build_tree(arr[mid+1..final])
+    root = Node.new(arr[mid], left_tree, right_tree)
+    root
+  end
+
+  #arr = [1,2,3,4]
+  #build_tree([1,2,3,4])
+  #build_tree([1,2])
+  #build_tree([1])
 end
