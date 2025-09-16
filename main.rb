6b3d4e439f4acfa9c1d002b4cbe4365a0d546749
Alejandro <chapale2000@gmail.com>
2025-09-16 18:40:01 -0300

    build_tree and pretty_print done!

---
+++ main.rb
@@ -0,0 +1,4 @@
+require "./tree.rb"
+
+new_tree = Tree.new([2,1,3])
+new_tree.pretty_print
--- tree.rb
+++ tree.rb
@@ -1,22 +1,36 @@
+require "./node.rb"
 class Tree 
-  
   def initialize(arr)
-    @root = build_tree(arr.sort)
+    clean_array = prepare_array(arr)
+    @root = build_tree(clean_array, 0, clean_array.length - 1)
   end
 
-  def build_tree(arr)
-    start = 0
-    final = arr.length - 1
-    return if start > final
-    mid = ((start + final)/2).floor
-    left_tree = build_tree(arr[start..mid])
-    right_tree = build_tree(arr[mid+1..final])
-    root = Node.new(arr[mid], left_tree, right_tree)
+  def prepare_array(arr)
+    arr.uniq.sort
+  end
+
+  def build_tree(arr, start, final)
+    return nil if start > final
+    mid = ((start + final) / 2).floor
+    root = Node.new(arr[mid])
+    root.set_left(build_tree(arr, start, mid-1))
+    root.set_right(build_tree(arr,mid+1,final))
     root
   end
 
-  #arr = [1,2,3,4]
-  #build_tree([1,2,3,4])
-  #build_tree([1,2])
-  #build_tree([1])
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_node, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_node
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left_node, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_node
+  end
+
+
+  #arr = [1,2]
+  #build_tree([1,2], 0, 1)
+  #mid = 0; root = 1;
+  #root.set_left = build_tree([0..-1], 0, -1)
+  #return nil
+  #root.set_right = build_tree([1..1], 1, 1)
+  #mid = 1; root = 2
+  #root.set_left = build_tree()
 end
